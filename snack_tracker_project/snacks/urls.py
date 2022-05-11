4e62f27684d46aed37b9abdc9198d324391cb95d
amaniq88 <amani.musallamq88@gmail.com>
2022-05-11 13:28:53 +0300

    test added

--- snack_tracker_project/snacks/urls.py
+++ snack_tracker_project/snacks/urls.py
@@ -1,6 +1,8 @@
-from django.urls import URLPattern, path
-from .views import SnackListview
+from django.urls import  path
+from .views import SnackListview, snackDetailView
 
 urlpatterns = [
-    path('snack_list', SnackListview.as_view(), name='snack_list')
+    path('snack_list', SnackListview.as_view(), name='snack_list'),
+    path('snack_detail/<int:pk>', snackDetailView.as_view(), name='snack_detail'),
+
 ]
--- snack_tracker_project/snacks/views.py
+++ snack_tracker_project/snacks/views.py
@@ -1,10 +1,17 @@
-from msilib.schema import ListView
 from pyexpat import model
+from re import template
+from statistics import mode
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Snack
 
 class SnackListview(ListView):
     template_name = "snack_list.html"
     model = Snack
+    contex_object_name = "snack_list"
+
+class snackDetailView(DetailView):
+    template_name = "snack_detail.html"
+    model = Snack
+
 
